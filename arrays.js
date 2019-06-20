f9620f08a52493ab788c449db0fee3d1590d460e
imperative-robot-4943 <drccamp32@gmail.com>
2019-06-20 05:25:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,25 @@
- function addElementToBeginningOfArray() {
-   
+var chocolateBars = [
+  "snickers","hundred grand","kitkat","skittles"];
+  
+  function addElementToBeginningOfArray(array, element) {
+ return [element,...array]  
  }
 
 
-function destructivelyAddElementToBeginningOfArray() {
- 
+function destructivelyAddElementToBeginningOfArray(array, element) {
+array.unshift(element) 
+return array 
 }
+
+function addElementToEndOfArray(array, element){
+  (array,...element)
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array,element){
+  return array.push(element)
+}
+
+
+
+
